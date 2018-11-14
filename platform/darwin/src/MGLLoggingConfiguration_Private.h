f50fe6b297d928c03c73ce006307692b1d5ff1dd
Fabian Guerra <fabian.guerra@mapbox.com>
2018-11-14 09:58:43 -0800

    [ios, macos] Remove verbose logging for release builds.

--- platform/darwin/src/MGLLoggingConfiguration_Private.h
+++ platform/darwin/src/MGLLoggingConfiguration_Private.h
@@ -7,15 +7,14 @@ NS_INLINE NSString *MGLStringFromBOOL(BOOL value) {
 #ifdef MGL_DISABLE_LOGGING
 #define MGLLogInfo(...)
 #define MGLLogDebug(...)
-#define MGLLogError(...)
-#define MGLLogFault(...)
 
 #else
 #define MGLLogInfo(message, ...)  MGLLogWithType(MGLLoggingLevelInfo, __PRETTY_FUNCTION__, __LINE__, message, ##__VA_ARGS__)
 #define MGLLogDebug(message, ...) MGLLogWithType(MGLLoggingLevelDebug, __PRETTY_FUNCTION__, __LINE__, message, ##__VA_ARGS__)
+#endif
+
 #define MGLLogError(message, ...) MGLLogWithType(MGLLoggingLevelError, __PRETTY_FUNCTION__, __LINE__, message, ##__VA_ARGS__)
 #define MGLLogFault(message, ...) MGLLogWithType(MGLLoggingLevelFault, __PRETTY_FUNCTION__, __LINE__, message, ##__VA_ARGS__)
-#endif
 
 #define MGLLogWithType(type, function, line, message, ...) \
 { \
